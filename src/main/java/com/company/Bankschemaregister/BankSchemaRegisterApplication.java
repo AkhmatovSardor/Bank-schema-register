7f3ec908f679c5b1822b9b853906e0d602972337
AkhmatovSardor <sardor.akhmatov0969@gmail.com>
2023-08-28 14:45:42 +0500

    first commit

---
+++ src/main/java/com/company/Bankschemaregister/BankSchemaRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.company.Bankschemaregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSchemaRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSchemaRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Bankschemaregister/BankSchemaRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Bankschemaregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSchemaRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
